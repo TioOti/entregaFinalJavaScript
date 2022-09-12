2047e6bddbf9d274cb55588dd612745a93ed1557
Octavio Ripoll <octavio.ripoll25@hotmail.com>
2022-09-11 23:10:40 -0300

    searchBarImplementada

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ function cargarDatos(){
         listaHerramientas.push(new Producto(producto.nombre,producto.precio,producto.stock,producto.categoria,producto.codigoProducto,producto.img,))
     });
     renderizarCards(listaHerramientas)
-    
+    eventoBusqueda(listaHerramientas)
     })
 }
 
@@ -287,71 +287,19 @@ let botonvaciar = document.getElementById("boton-vaciar")
 botonvaciar.addEventListener("click", vaciarCarrito)
 
  // Funcion de la Search Bar Starts
- 
-/* let searchBar = document.getElementById("input_text")
-let listaHerramientasBusqueda =[];
 
-searchBar.addEventListener('input', (e) => {
-    const textoBusqueda = e.target.value.toLowerCase();
-    console.log(textoBusqueda)
-  
-    let herramientasFiltradas = listaHerramientasBusqueda.filter((herramienta) => {
-        return (
-            herramienta.nombre.toLowerCase().includes(textoBusqueda)
-        );
-    });
-    renderizarCards(herramientasFiltradas);
-});
- */
+ function eventoBusqueda (listaHerramientas){
+    let searchBar = document.getElementById("input_text")
+    searchBar.addEventListener('keyup', (e) => {
+        const textoBusqueda = e.target.value.toLowerCase();
+        let herramientasFiltradas = listaHerramientas.filter((herramienta) =>{
+            return herramienta.nombre.toLowerCase().includes(textoBusqueda)
+        })
+        card_container.innerHTML = ""
+        renderizarCards(herramientasFiltradas)
+    })
+}
 
-
-/*
-// Harry Potter API
-
-            const charactersList = document.getElementById('charactersList');
-            const searchBar = document.getElementById('searchBar');
-            let hpCharacters = [];
-
-            searchBar.addEventListener('keyup', (e) => {
-                const searchString = e.target.value.toLowerCase();
-
-                const filteredCharacters = hpCharacters.filter((character) => {
-                    return (
-                        character.name.toLowerCase().includes(searchString) ||
-                        character.house.toLowerCase().includes(searchString)
-                    );
-                });
-                displayCharacters(filteredCharacters);
-            });
-
-            const loadCharacters = async () => {
-                try {
-                    const res = await fetch('https://hp-api.herokuapp.com/api/characters');
-                    hpCharacters = await res.json();
-                    displayCharacters(hpCharacters);
-                } catch (err) {
-                    console.error(err);
-                }
-            };
-
-            const displayCharacters = (characters) => {
-                const htmlString = characters
-                    .map((character) => {
-                        return `
-                        <li class="character">
-                            <h2>${character.name}</h2>
-                            <p>House: ${character.house}</p>
-                            <img src="${character.image}"></img>
-                        </li>
-                    `;
-                    })
-                    .join('');
-                charactersList.innerHTML = htmlString;
-            };
-
-            loadCharacters();   
-
- */
 // Funcion de la Search Bar fin
 
 
